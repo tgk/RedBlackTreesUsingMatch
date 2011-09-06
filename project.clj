1539917227c3d2208d5db7ac902a87108feb6da7
tgk <tgk@cs.au.dk>
2011-09-06 19:31:38 +0200

    Added run method

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [org.clojure/core.match "0.2.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
+  :main redblack.core)
